8fc7ddb43c19aac35d15ab145f977ba293775228
Rob Brown <cagao@cpan.org>
2012-03-21 09:03:26 +0000

    Import of CAGAO/HTML-FormHandlerX-Form-Login-0.04 from CPAN.
    gitpan-cpan-distribution: HTML-FormHandlerX-Form-Login
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         CAGAO/HTML-FormHandlerX-Form-Login-0.04.tar.gz
    gitpan-cpan-author:       CAGAO
    gitpan-cpan-maturity:     released

--- lib/HTML/FormHandlerX/Form/Login.pm
+++ lib/HTML/FormHandlerX/Form/Login.pm
@@ -14,11 +14,11 @@ HTML::FormHandlerX::Form::Login - An HTML::FormHandler login form.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.04
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
